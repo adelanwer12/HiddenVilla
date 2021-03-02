c8d9e7c3b43590ff9bc55bc4de4d98c7b6e5955e
adel ibrahim <adelanwer12@yahoo.com>
2021-03-03 00:37:47 +0200

    Add project files.

---
+++ HiddenVilla_Server/Models/BlazorRoom.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace HiddenVilla_Server.Models
+{
+    public class BlazorRoom
+    {
+        public Guid Id { get; set; }
+        public string RoomName { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public List<BlazorRoomProp> RoomProps { get; set; }
+    }
+}
---
+++ HiddenVilla_Server/Models/BlazorRoomProp.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HiddenVilla_Server.Models
+{
+    public class BlazorRoomProp
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
